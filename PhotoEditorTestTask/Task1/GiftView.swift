d7ba5b2105b5d55efd44efa1a6ec84bf2cf82f76
Kristina Grebneva <kristina.o.grebneva@gmail.com>
2025-08-22 16:22:14 +0200

    reduced iOS target to iOS 12

---
+++ PhotoEditorTestTask/Task1/GiftView.swift
@@ -0,0 +1,20 @@
+//
+//  GiftView.swift
+//  PhotoEditorTestTask
+//
+//  Created by Kristina Grebneva on 22.08.2025.
+//
+
+import UIKit
+
+class GiftView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
